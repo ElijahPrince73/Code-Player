8c0cf367c1a26ebeed6fa0fe0b6861a437b77bd9
Elijah Prince <elijahprince73@gmail.com>
2016-07-09 16:53:05 -0700

    added in code to make code player and test my code

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-var windowHeight = $(window).height();
+ $(function() {
 
-var menuBarHeight=$("#menuBar").height();
+ var height=$( window ).height()-40;
 
-var codeContainerHeight=windowHeight-menuBarHeight;
-$(".codeContainer").height(codeContainerHeight+"px");
+ $(".codecontainer").css("height",height+"px");
+ $(".selector").click(function() {
+
+ $( this ).toggleClass( "selected" );
+
+ 	 	 var id=$(this).attr("name");
+ 	 	 	
+ 	 	 $("#"+id+"Container").toggle();
+ 	 	 	
+ 	 	 var number = $('.codecontainer').filter(function() {
+ return $(this).css('display') !== 'none';
+}).length;
+	 	 var width=100/number;
+			
+	 	 $(".codecontainer").css("width",width+"%"); 		 	
+
+ });
+ 	 	
+ 	 	
+ $("#run").click(function() {
+ 	 	
+
+ 	 	 $('#resultFrame').contents().find('html').html("<style>"+$
+('#css').val()+"</style>"+$("#html").val());
+ 	 	 	
+ document.getElementById('resultFrame').contentWindow.eval( $
+('#js').val() );
+ });
+
+
+ });
